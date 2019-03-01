c18aed726f77201e600180d647d667fe2dc6037a
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-03-01 10:36:40 +0000

    Get Smurfs Data From Server

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+const apiURL = 'http://localhost:3333/smurfs';
 
 class App extends Component {
   constructor(props) {
@@ -11,9 +13,47 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    this.getSmurfs();
+  }
+
+  //====== API REQUEST CODE ======//
+
+  request = (url, method, successCallback, failCallback, payload) => {
+    let receivedData = null;
+    switch (method) {
+    case 'GET':
+      axios.get(url)
+        .then((res) => {
+          receivedData = res.data;
+          successCallback(res.data);
+        })
+        .catch((res) => {
+          receivedData = res.data;
+          failCallback(res.data);
+        })
+      break;
+    default:
+      console.error('request failed');
+
+      return receivedData;
+    }
+  }
+
+  getSmurfs = () => {
+    const setSmurfsToState = (data) => this.setDataToState(data, ['smurfs']);
+    this.request(apiURL, 'GET', setSmurfsToState);
+  }
+
+  //====== STATE UTILITY FUNCTIONS ======//
+
+  setDataToState = (data, stateKeysArray) => {
+    for (let key in stateKeysArray) {
+      this.setState({ [stateKeysArray[key]]: data });
+    }
+  }
+
   render() {
     return (
       <div className="App">
